226144a06015e341ca0470d5f6d27e8f95a2c785
ducminh <ducminhle231@gmail.com>
2021-07-26 00:43:35 +0700

    create 404 not found page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import "./Header.scss"
-import {  Col, Container, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 Header.propTypes = {};
 
@@ -9,29 +9,29 @@ function Header() {
     return (
         <header className="header">
             <Container>
-            <Row className="justify-content-between">
-                <Col xs="auto">
-                <a 
-                href="/" 
-                className="header__link header__title"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    Photo App
-                </a>
-                </Col>
-                <Col xs="auto">
-                <NavLink
-                className="header__link"
-                to="/photos"
-                activeClassName="header__link--active"
-                >
-                    Redux project
-                </NavLink>
-                </Col>
-            </Row>
+                <Row className="justify-content-between">
+                    <Col xs="auto">
+                        <a
+                            href="/"
+                            className="header__link header__title"
+                        // target="_blank"
+                        // rel="noopener noreferrer"
+                        >
+                            Photo App
+                        </a>
+                    </Col>
+                    <Col xs="auto">
+                        <NavLink
+                            className="header__link"
+                            to="/photos"
+                            activeClassName="header__link--active"
+                        >
+                            Redux project
+                        </NavLink>
+                    </Col>
+                </Row>
             </Container>
-           
+
         </header>
     )
 }
